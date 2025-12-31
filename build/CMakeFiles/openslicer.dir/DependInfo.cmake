8174ea5fd22d72866cfef623a6c21d6cd09f5381
Sonu Linux <sonukumarzeroone@gmail.com>
2025-12-31 22:03:29 +0530

    create: stl loading is working

---
+++ build/CMakeFiles/openslicer.dir/DependInfo.cmake
@@ -0,0 +1,20 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/sonu/mine-projects/slicer_v1/app/main.cpp" "CMakeFiles/openslicer.dir/app/main.cpp.o" "gcc" "CMakeFiles/openslicer.dir/app/main.cpp.o.d"
+  "/home/sonu/mine-projects/slicer_v1/engine/mesh/Mesh.cpp" "CMakeFiles/openslicer.dir/engine/mesh/Mesh.cpp.o" "gcc" "CMakeFiles/openslicer.dir/engine/mesh/Mesh.cpp.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/CMakeFiles/openslicer.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/openslicer.dir/app/main.cpp.o"
+  "CMakeFiles/openslicer.dir/app/main.cpp.o.d"
+  "CMakeFiles/openslicer.dir/engine/mesh/Mesh.cpp.o"
+  "CMakeFiles/openslicer.dir/engine/mesh/Mesh.cpp.o.d"
+  "openslicer"
+  "openslicer.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/openslicer.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
